dea8acfe0a52a20e7c68e6c03d6380477e3516a8
Andrew Coleburn <arcoleburn@gmail.com>
2020-10-23 15:54:10 -0400

    added store file

---
+++ src/store.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const bookmarks = [
+  {
+    id: 'a',
+    title: 'Google',
+    url: 'https://google.com',
+    description: 'the most popular search engine',
+    rating: 5,
+  },
+];
+
+module.exports = { bookmarks };
