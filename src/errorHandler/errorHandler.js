942d69308a2a97ed04a8808d0c03cd272ae28eeb
Andrew Coleburn <arcoleburn@gmail.com>
2020-10-29 12:31:41 -0400

    added first migration files and config postrgrator

--- src/errorHandler/errorHandler.js
+++ src/errorHandler/errorHandler.js
@@ -6,7 +6,6 @@ function errorHandler(error, req, res, next) {
   if (process.env.NODE_ENV === 'production') {
     response = { error: { message: 'server error' } };
   } else {
-    console.log(error);
     response = { message: error.messager, error };
   }
   res.status(500).json(response);
